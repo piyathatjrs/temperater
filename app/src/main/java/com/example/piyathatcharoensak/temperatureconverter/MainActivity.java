d1c3af3ece6d1962e960a79eddd944c03d294556
piyathatjrs <piyathatjrs@gmail.com>
2018-10-27 22:14:44 +0700

    Homework1

---
+++ app/src/main/java/com/example/piyathatcharoensak/temperatureconverter/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.piyathatcharoensak.temperatureconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.piyathatcharoensak.temperatureconverter.Model.Body;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button c_to_f = findViewById(R.id.CF);
+        c_to_f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.Input);
+                int number = Integer.parseInt(input.getText().toString());
+                Body body = new Body(number);
+                final TextView output = findViewById(R.id.Output);
+                String show = Integer.toString(body.tranform_c_to_f());
+                output.setText(show);
+            }
+        });
+        Button f_to_c = findViewById(R.id.FC);
+        f_to_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.Input);
+                int number = Integer.parseInt(input.getText().toString());
+                Body body = new Body(number);
+                final TextView output = findViewById(R.id.Output);
+                String show = String.format("%.2f", body.tranform_f_to_c());
+                output.setText(show);
+            }
+        });
+    }
+}
